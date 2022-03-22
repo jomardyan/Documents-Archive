4ba93feb04c27bf1bd602a91918931a60d07ce9d
Hayk Jomardyan <jomardyan@outlook.com>
2022-03-22 21:22:11 +0100

    Relase

---
+++ DocEnum.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace ArchiveLogic
+{
+   
+ 
+ public enum DocumentType
+    {
+        PDF,
+        DOC,
+        
+    }
+}
---
+++ Interfaces.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ArchiveLogic
+{
+   
+
+    public interface IDocument
+    {
+        public DocumentType Type { get; set; }  
+        public string Name { get; set; }
+        // ID = GUID
+        public string ID { get; set; }
+
+        public string GetID(); 
+
+    }
+    interface IBox
+    {
+        public  MyProperty { get; set; }
+    }
+
+
+    interface ILocation
+    {
+        
+    }
+
+    interface IDocIndex
+    {
+        public string BoxID { get; set; }
+        public string DocID { get; set; }
+    }
+
+}
